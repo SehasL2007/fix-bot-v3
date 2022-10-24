58dca98b531baefd0da27bd387816ece3f3bf940
SehasL2007 <116531761+SehasL2007@users.noreply.github.com>
2022-10-24 11:55:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -138,12 +138,12 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4 」' //Main Watermark
+global.wm = '「 𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4' //Main Watermark
 global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ 𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4 '
+global.wm3 = '⫹⫺ 𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4'
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | By WH-MODS-DEV`
+global.titlebot = `⫹⫺ WhatsApp Bot | By ⫹⫺ 𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4
 global.packname = '☂︎ ᴄʀᴇᴀᴛᴇᴅ ʙʏ'
 global.author = '                「𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4  あ」'
 
@@ -636,7 +636,7 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = 'WH-MODS-BOT-V1'
+global.stickpack = '⫹⫺ 𝚂𝙴𝙷𝙰𝚂 𝙼𝙳 𝚅4'
 global.stickauth = `By ©WH-MODS-DEV`
 
 global.multiplier = 38 // The higher, The harder levelup
